dafe24336438ed0ca62abf45e953da1b78026a30
surendhar <surendhark240@gmail.com>
2024-07-14 21:38:59 +0530

    Updated right panel search bar

--- src/Components/BlogEntries.jsx
+++ src/Components/BlogEntries.jsx
@@ -1,12 +1,25 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import './BlogEntries.css';
 import { BlogContext } from './BlogContext';
 
 const BlogEntries = () => {
   const { blogs } = useContext(BlogContext);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const filteredEntries = blogs.filter(entry =>
+    entry.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="blog-entries">
-      {blogs.map((blog) => (
+      <input
+        type="text"
+        placeholder="Search by title..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="search-bar"
+      />
+      {filteredEntries.map((blog) => (
         <div key={blog.id} className="blog-entry">
           <h3>{blog.title}</h3>
           {blog.image && (
@@ -14,7 +27,7 @@ const BlogEntries = () => {
           )}
           <p>{blog.content}</p>
           <h4>~{blog.name}</h4>
-          </div>
+        </div>
       ))}
     </div>
   );
